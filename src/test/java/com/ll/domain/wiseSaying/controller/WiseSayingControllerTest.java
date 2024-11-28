700dd56d11cdf0e119c148629711c389e9795d7a
Hyeonsuk <vkdnjdjxor@naver.com>
2024-11-28 11:55:09 +0900

    9단계 명언 게시판 테스트케이스 작성[TDD]

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -1,6 +1,9 @@
 package com.ll.domain.wiseSaying.controller;
 
 import com.ll.AppTest;
+import com.ll.domain.wiseSaying.repository.WiseSayingFileRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +11,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class WiseSayingControllerTest {
+    @BeforeEach
+    public void beforeEach() {
+        WiseSayingFileRepository.dropTable();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        WiseSayingFileRepository.dropTable();
+    }
+
     @Test
     @DisplayName("== 명언 앱 ==")
     public void t1() {
@@ -107,9 +120,9 @@ public class WiseSayingControllerTest {
         assertThat(output)
                 .contains("번호  /  작가  /  명언")
                 .contains("--------------------")
-                .contains("3  /  이순신  /  나의 죽음을 적들에게 알리지 말라!")
-                .contains("2  /  작자미상  /  과거에 집착하지 마라.")
-                .contains("1  /  작자미상  /  현재를 사랑하라.");
+                .contains("3   /   이순신   /   나의 죽음을 적들에게 알리지 말라!")
+                .contains("2   /   작자미상   /   과거에 집착하지 마라.")
+                .contains("1   /   작자미상   /   현재를 사랑하라.");
     }
 
     @Test
@@ -127,8 +140,8 @@ public class WiseSayingControllerTest {
                 """);
 
         assertThat(output)
-                .contains("2  /  작자미상  /  과거에 집착하지 마라.")
-                .doesNotContain("1  /  작자미상  /  현재를 사랑하라.");
+                .contains("2   /   작자미상   /   과거에 집착하지 마라.")
+                .doesNotContain("1   /   작자미상   /   현재를 사랑하라.");
     }
 
     @Test
@@ -186,7 +199,7 @@ public class WiseSayingControllerTest {
                 """);
 
         assertThat(output)
-                .contains("2  /  홍길동  /  현재와 자신을 사랑하라.");
+                .contains("2   /   홍길동   /   현재와 자신을 사랑하라.");
     }
 
 }
--- src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -2,27 +2,26 @@ package com.ll.domain.wiseSaying.repository;
 
 import com.ll.domain.wiseSaying.entity.WiseSaying;
 import com.ll.standard.util.Util;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
+import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class WiseSayingFileRepositoryTest {
-    private final WiseSayingRepository wiseSayingRepository = new WiseSayingFileRepository();
+public class WiseSayingFileRepositoryTest {
+    private final WiseSayingFileRepository wiseSayingRepository = new WiseSayingFileRepository();
 
-    @BeforeAll
-    public static void beforeAll() {
-        Util.file.rmdir("db");
-        Util.file.mkdir("db");
+    @BeforeEach
+    public void beforeEach() {
+        Util.file.rmdir(WiseSayingFileRepository.DIR_PATH);
     }
 
-    @AfterAll
-    public static void afterAll() {
-        Util.file.rmdir("db");
+    @AfterEach
+    public void afterEach() {
+        Util.file.rmdir(WiseSayingFileRepository.DIR_PATH);
     }
 
     @Test
@@ -31,16 +30,83 @@ class WiseSayingFileRepositoryTest {
         WiseSaying wiseSaying = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
         wiseSayingRepository.save(wiseSaying);
 
-        String filePath = "db/test/wiseSaying/1.json";
+        Optional<WiseSaying> opWiseSaying = wiseSayingRepository.findById(wiseSaying.getId());
+
+        assertThat(
+                opWiseSaying.get()
+        ).isEqualTo(wiseSaying);
+    }
+
+    @Test
+    @DisplayName("명언 삭제")
+    public void t2() {
+        WiseSaying wiseSaying = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingRepository.save(wiseSaying);
+
+        wiseSayingRepository.deleteById(wiseSaying.getId());
+
+        String filePath = WiseSayingFileRepository.getFilePath(wiseSaying.getId());
 
         assertThat(
                 Util.file.exists(filePath)
-        ).isTrue();
+        ).isFalse();
+    }
 
-        String jsonStr = Util.file.get(filePath, "");
-        Map<String, Object> wiseSayingMap = Util.json.toMap(jsonStr);
-        WiseSaying wiseSayingRestored = new WiseSaying(wiseSayingMap);
+    @Test
+    @DisplayName("명언 단건조회")
+    public void t3() {
+        WiseSaying wiseSaying = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingRepository.save(wiseSaying);
 
-        assertThat(wiseSayingRestored).isEqualTo(wiseSaying);
+        Optional<WiseSaying> opWiseSaying = wiseSayingRepository.findById(wiseSaying.getId());
+
+        assertThat(
+                opWiseSaying.get()
+        ).isEqualTo(wiseSaying);
+    }
+
+    @Test
+    @DisplayName("명언 다건조회")
+    public void t4() {
+        WiseSaying wiseSaying1 = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingRepository.save(wiseSaying1);
+
+        WiseSaying wiseSaying2 = new WiseSaying(0, "나의 삶의 가치는 나의 결정에 달려있다.", "아인슈타인");
+        wiseSayingRepository.save(wiseSaying2);
+
+        assertThat(
+                wiseSayingRepository.findAll()
+        ).containsExactlyInAnyOrder(wiseSaying1, wiseSaying2);
+    }
+
+    @Test
+    @DisplayName("lastId.txt 생성")
+    public void t5() {
+        WiseSaying wiseSaying1 = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingRepository.save(wiseSaying1);
+
+        int lastId = wiseSayingRepository.getLastId();
+
+        assertThat(
+                lastId
+        ).isEqualTo(wiseSaying1.getId());
+    }
+
+    @Test
+    @DisplayName("명언 수정")
+    public void t6() {
+        WiseSaying wiseSaying = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingRepository.save(wiseSaying);
+
+        wiseSaying.setContent("나의 삶의 가치는 나의 결정에 달려있다.");
+        wiseSaying.setAuthor("아인슈타인");
+
+        wiseSayingRepository.save(wiseSaying);
+
+        Optional<WiseSaying> opWiseSaying = wiseSayingRepository.findById(wiseSaying.getId());
+
+        assertThat(
+                opWiseSaying.get()
+        ).isEqualTo(wiseSaying);
     }
 }
